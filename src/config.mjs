6958c8826cfc05b74c91e50ab9c917d285850e6c
Benjamin <benjamin@awesomebible.de>
2023-08-11 21:31:15 +0200

    Update og image

--- src/config.mjs
+++ src/config.mjs
@@ -2,10 +2,10 @@ export default {
     title: "Homepage",
     titleSuffix: "",
     description: "Ramblings & Musings by me, Benjamin.",
-    defaultOgImage: "./og.jpg",
+    defaultOgImage: "https://bendaha.eu.org/og.jpg?v=1",
     latestPosts: 3,
     postsPerPage: 3,
-    url: "https://bendaha.eu.org",
+    url: "https://bendaha.eu.org/",
     social: {
         'dribbble': false,
         'facebook': false,
