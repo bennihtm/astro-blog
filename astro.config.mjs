377e1849cd3a1e41b790d05659013c72994d5920
Benjamin <benjamin@awesomebible.de>
2023-08-12 17:21:09 +0200

    Add robots.txt & sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: "https://bendaha.eu.org"
 });
