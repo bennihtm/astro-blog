93c865446dec035066948a596de7dd2183ea28d9
Benjamin <benjamin@awesomebible.de>
2023-08-19 12:11:38 +0200

    Add Partytown & Cf Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), partytown()],
   site: "https://bendaha.eu.org"
 });
