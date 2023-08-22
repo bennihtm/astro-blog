a1fb6764a74010f82a1f787245f5186ed5c6da69
Benjamin <benjamin@awesomebible.de>
2023-08-22 17:53:57 +0200

    Add Umami Analytics with Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     assets: true
   },
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), partytown()],
   site: "https://bendaha.eu.org"
 });
