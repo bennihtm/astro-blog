14f09882ca7984644ac49689c79987e5b08bb8be
Benjamin <benjamin@awesomebible.de>
2023-10-27 19:58:06 +0200

    Remove Partytown 🎉

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), partytown()],
+  integrations: [tailwind(), mdx(), sitemap()],
   site: "https://bendaha.eu.org"
 });
